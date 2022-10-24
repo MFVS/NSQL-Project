fa9f234c06f47eb82076886c0ac1331fd6d03d3e
yoptohlejepeta <peta.kotlan@seznam.cz>
2022-10-24 09:50:39 +0200

    added sqlalchemy, templates

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 app = Flask(__name__)                 #http://localhost:5000
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=True, nullable=False)
@@ -16,24 +19,37 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
       user = request.form['nm']
-      # password = request.form['pw']
-      return render_template('home.html',name = user)
-    else:
-      user = request.args.get('nm')
-      # password = request.args.get('pw')
-      return render_template('login.html',name = user)
+      password = request.form['pw']
+      #email
+      new_user = Users(username = user, password = password)
 
-@app.route('/home')
+      try:
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+      except:
+        return 'ERROR'
+
+    else:
+      users = Users.query.order_by(Users.id).all()
+      return render_template('login.html', users = users)
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+  return render_template('register.html')
+
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/number/<int:number>')
-def hello_ok(number):
-    return render_template('cislo.html', value = number)
+@app.route('/database')
+def database():
+  users = Users.query.order_by(Users.id).all()
+  return render_template('database.html', users = users)
 
 @app.route('/json')
 def hello_js():
