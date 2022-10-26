875ef4a9e5a42ae6a9a818820f24babb0c7f8bcf
yoptohlejepeta <peta.kotlan@seznam.cz>
2022-10-26 18:05:29 +0200

    added wtf, login

--- app.py
+++ app.py
@@ -1,59 +1,89 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, UserMixin
+from wtforms import Form, StringField, PasswordField, validators
 
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
-app = Flask(__name__)                 #http://localhost:5000
+app = Flask(__name__)  # http://localhost:5000
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SECRET_KEY"] = "secretkey"
 db.init_app(app)
+login_manager.init_app(app)
 
-class Users(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
+
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+
 
 with app.app_context():
     db.create_all()
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+class RegistrationForm(Form):
+    username = StringField("", [validators.Length(min=4, max=25)])
+    email = StringField("", [validators.Length(min=6, max=35)])
+    password = PasswordField(
+        "",
+        [
+            validators.DataRequired(),
+            validators.EqualTo("confirm", message="Passwords must match"),
+        ],
+    )
+    confirm = PasswordField("")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-      user = request.form['nm']
-      password = request.form['pw']
-      #email
-      new_user = Users(username = user, password = password)
+    return render_template("login.html")
 
-      try:
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect('/')
-      except:
-        return 'ERROR'
 
-    else:
-      users = Users.query.order_by(Users.id).all()
-      return render_template('login.html', users = users)
-
-@app.route('/register', methods = ['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def register():
-  return render_template('register.html')
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        new_user = Users(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            return "ERROR"
+        login_user(new_user)
+        return redirect(url_for("home"))
+    return render_template("register.html", form=form)
 
-@app.route('/')
+
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/database')
+
+@app.route("/database")
 def database():
-  users = Users.query.order_by(Users.id).all()
-  return render_template('database.html', users = users)
+    users = Users.query.order_by(Users.id).all()
+    return render_template("database.html", users=users)
 
-@app.route('/json')
+
+@app.route("/json")
 def hello_js():
-  return render_template('js.html')
+    return render_template("js.html")
 
-if __name__== "__main__":
-    app.run(debug=True, port = 5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)  # set FLASK_DEBUG=1
