4a3becda56af178e0d8eb798f64c771c662f0543
nexovec <hraccesky@gmail.com>
2022-12-16 21:49:32 +0100

    FTFY, forgot to include original commit message lol noob nexo

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, logout_user, UserMixin, login_required
+from flask_login import LoginManager, login_user, logout_user, UserMixin, login_required, current_user
 from wtforms import Form, StringField, PasswordField, validators
+from flask_mail import Mail, Message
+import redis
+from datetime import timedelta
 
 db = SQLAlchemy()
+mail = Mail()
 login_manager = LoginManager()
 
 app = Flask(__name__)
 
+r = redis.Redis(host = 'redis', port = 6379, decode_responses=True)
 
-# docker run --name kosmonauti -e POSTGRES_USER=Buzz -e POSTGRES_PASSWORD=Raketak -p 5432:5432 -d postgres
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgrespw@databasepg:5432'
-app.config["SECRET_KEY"] = "secretkey"
+app.config["SECRET_KEY"] = "uhapw389a3ba30rai3b20sbj"
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'tm6990888@gmail.com'
+app.config['MAIL_PASSWORD'] = 'cjjhivnvbzmyvlgk'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail.init_app(app)
 db.init_app(app)
 login_manager.init_app(app)
 
@@ -21,8 +32,6 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 # Users
-
-
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -42,17 +51,21 @@ class RegistrationForm(Form):
         [validators.DataRequired(), validators.EqualTo(
             "confirm", message='Passwords must match.')],
     )
-    confirm = PasswordField("Confirm password: ")
-
+    confirm = PasswordField("Confirm password: ", [validators.Length(min=5, max=25)])
 
 class LoginForm(Form):
     username = StringField('Username: ', [validators.Length(min=4, max=15)])
     password = PasswordField('Password: ', [validators.DataRequired()])
 
+class AuthenticateForm(Form):
+    password = PasswordField('Code: ', [validators.DataRequired()])
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm(request.form)
     if request.method == "POST" and form.validate():
         user = Users.query.filter_by(username=form.username.data).first()
@@ -81,10 +94,24 @@ def register():
             db.session.commit()
         except:
             return "ERROR"
+        heslo = 123
+        r.setex(f"{new_user.username}", timedelta(minutes=1), value = heslo)
+        msg = Message('Authentication code.', sender = 'tm6990888@gmail.com', recipients = [f'{new_user.email}'])
+        msg.body = f"{heslo}"
+        mail.send(msg)
         login_user(new_user)
-        return redirect(url_for("home"))
+        return redirect(url_for("authentication"))
     return render_template("register.html", form=form)
 
+@app.route("/authentication", methods=["GET","POST"])
+def authentication():
+    form = AuthenticateForm(request.form)
+    if request.method == "POST" and form.validate():
+        if form.password.data == r.get(current_user.username):
+            return redirect(url_for("home"))
+        else:
+            return f'{r.get(current_user.username)}, {form.password.data}' 
+    return render_template("authentication.html", form=form)
 
 @app.route("/home")
 @login_required
@@ -104,6 +131,9 @@ def database():
     users = Users.query.order_by(Users.id).all()
     return render_template("database.html", users=users)
 
+# @app.route('/user', methods = ['GET','PUT','DELETE'])
+# def user_page():
+#     return render_template('user.html')
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
